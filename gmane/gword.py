5250b37d63bf71c625336148e78f50c63afa4639
Charles Severance <csev@umich.edu>
2013-04-09 00:23:25 -0400

    Initial commit of word capability

---
+++ gmane/gword.py
@@ -0,0 +1,54 @@
+import sqlite3
+import time
+import urllib
+import dateutil.parser as parser
+import zlib
+import string
+
+conn = sqlite3.connect('index.sqlite')
+conn.text_factory = str
+cur = conn.cursor()
+
+cur.execute('SELECT id, subject FROM Subjects')
+subjects = dict()
+for message_row in cur :
+    subjects[message_row[0]] = message_row[1]
+
+# cur.execute('SELECT id, guid,sender_id,subject_id,headers,body FROM Messages')
+cur.execute('SELECT subject_id FROM Messages')
+counts = dict()
+for message_row in cur :
+    text = subjects[message_row[0]]
+    text = text.translate(None, string.punctuation)
+    text = text.translate(None, '1234567890')
+    text = text.strip()
+    text = text.lower()
+    words = text.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
+x = sorted(counts, key=counts.get, reverse=True)
+highest = None
+lowest = None
+for k in x[:100]:
+    if highest is None or highest < counts[k] :
+        highest = counts[k]
+    if lowest is None or lowest > counts[k] :
+        lowest = counts[k]
+print highest,lowest
+bigsize = 70
+smallsize = 30
+
+fhand = open('gword.js','w')
+fhand.write("gword = [")
+first = True
+for k in x[:100]:
+    if not first : fhand.write( ",\n")
+    first = False
+    size = counts[k]
+    size = (size - lowest) / float(highest - lowest)
+    size = int((size * bigsize) + smallsize)
+    fhand.write("{text: '"+k+"', size: "+str(size)+"}")
+fhand.write( "\n];\n")
+
+print "Output written to gword.js"
